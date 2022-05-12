84325b887205a29aa73cda50592cd1f85361ac3f
Dmitry Petrov <e2m3x4@gmail.com>
2022-05-12 15:11:43 +0000

    fixing app and docker-compose

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 import json
+import os
 
 
 FILE_PATH = os.path.join(os.path.dirname(__file__), 'data.json')
@@ -7,11 +8,11 @@ FILE_PATH = os.path.join(os.path.dirname(__file__), 'data.json')
 
 app = Flask(__name__)
 
-
-@app.route('/'):
+@app.route('/')
+@app.route('/remarks')
 def index():
     with open(FILE_PATH, 'r') as fp:
-        data = json.dump(fp)
+        data = json.load(fp)
         
     return jsonify(data)
 
